39f002133a6083772a900c04e0d87a065e5e318d
Jonathan Arias Busto <UO283586@uniovi.es>
2023-06-05 15:02:22 +0200

    after tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :friends
+  # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  get "home/about" 
+  root "friends#index" # This is the same as get "/", to: "home#index" 
 end
